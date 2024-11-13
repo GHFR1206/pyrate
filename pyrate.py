f7a095457f7787e704585dd5a47fea11f8b4dadf
GHFR1206 <aghifari1206@gmail.com>
2024-11-13 07:50:42 +0700

    lil bit polishing on edit (unfinished = genre)

--- pyrate.py
+++ pyrate.py
@@ -22,11 +22,12 @@ def movie_list():
     film.index += 1
     print(tabulate(film, headers='keys', tablefmt='grid'))
 
-def create(name, year, desc):
+def create(name, year, genre):
     data = {
         'id': len(film)+1,
         'name': [name.title()],
         'year': [year],
+        'genre': genre,
         'watched': 'No',
         'rate': '-',
         'review': '-',
@@ -35,23 +36,22 @@ def create(name, year, desc):
     create = pd.concat([film, data_new], ignore_index=False)
     create.to_excel(file_path, index=False)
     
-def edit(id, name, year):
+def edit(id, name, year, genre):
     df.at[id-1, 'name'] = name
     df.at[id-1, 'year'] = year
+    df.at[id-1, 'genre'] = genre
     pd.DataFrame(df).to_excel(file_path, index=False)
     
 def delete(id):
     df.drop(id-1, inplace=True)
     pd.DataFrame(df).to_excel(file_path, index=False)
     
-def detail(name):    
-    data = df[df["name"] == name]
-    if data.empty:
-        return 0
+def search(name):    
+    data = df[df["name"].str.contains(name)]
     data.index+=1
     return data
 
-def rating(id, rate):
+def rating(id, rate, review):
     data = df[df["id"] == id]
     data.index += 1
     rateint = int(rate)
@@ -60,6 +60,7 @@ def rating(id, rate):
     
     data.at[id, 'rate'] = star
     data.at[id, 'watched'] = 'Yes'
+    data.at[id, 'review'] = review
     pd.DataFrame(data).to_excel(file_path, index=False)
     
 while True:
@@ -84,9 +85,7 @@ what'u want?
         print("""
 Action:
 [1] Add movie
-[2] Delete movie
-[3] Edit Movie
-[4] Search movie
+[2] Search movie
 [0] Back
               """)
         user = int(input("> "))
@@ -94,51 +93,24 @@ Action:
         
         # create
         if user == 1:
-            print("insert movie name then year")
-            name = input('Movie name > ')
-            year = input('Movie year > ')
-            create(name, year)
-            print("Completed!")
-            time.sleep(2)
-            continue
-            
-        # delete
-        if user == 2:
-            print("Select movie you want to delete")
-            id = int(input("> "))
-            print(f"you sure want to delete '{df.iloc[id-1]['name']}'? y/n")
-            confirm = input("> ")
-            confirm.lower
-            if confirm == "y":
-                delete(id)
-                print("Deleted successfully!")
-                time.sleep(2)
-                continue
-            else:
-                print("Cancelling..")
-                time.sleep(2)
-                continue
-            
-        # update
-        if user == 3:
-            print("Select movie you want to edit")
-            id = int(input("> "))
-            print("Input new movie name")
-            name = input("> ")
-            print("Input new movie year")
-            year = input("> ")
-                        
-            edit(id, name, year)
-            print("Edited succesfully!")
+            print("insert movie name")
+            name = input('> ')
+            print("\ninsert movie year")
+            year = input('> ')
+            print("\ninsert movie genre")
+            genre = input('> ')
+            create(name, year, genre)
+            print("\nCompleted!")
             time.sleep(2)
             continue
                        
-        if user == 4:
+        if user == 2:
             print("Type movie name")
             name = input("> ").title()
-            data = detail(name)
-            if data.empty:
-                print("Movie not found!")
+            data = search(name)
+            data = pd.DataFrame(data)
+            if search(name).empty:
+                print("\nMovie not found!")
                 time.sleep(2)
                 continue
             
@@ -146,18 +118,61 @@ Action:
             print(tabulate(data, headers='keys', tablefmt='grid'))
             print(f"""
 What are you gonna do with that?
-[1] Rate
-[2] Review 
+[1] Edit
+[2] Delete
+[3] Rate n Rate it!
 [0] Back
                   """)
             user = int(input("> "))
             
+            # update
             if user == 1:
-                print("Input rate number 1-5!")
+                print("Insert new movie name (Leave blank if no change)")
+                name = input("> ")
+                print("\nInsert new movie year (Leave blank if no change)")
+                year = input("> ")
+                print("\nInsert new movie genre (Leave blank if no change)")
+                genre = input("> ")
+                
+                if len(name) == 0:
+                    name = data.iloc[:]['name'].values[0]
+                if len(year) == 0:
+                    year = data.iloc[:]['year'].values[0]
+                if len(genre) == 0:
+                    genre = data.iloc[:]['genre'].values[0]
+                            
+                edit(data.iloc[:]['id'].values[0], name, year, genre)
+                print("\nEdited succesfully!")
+                time.sleep(2)
+                continue
+            
+            # delete
+            if user == 2:
+                print(f"you sure want to delete '{data.at[1, 'name']}'? y/n")
+                confirm = input("> ")
+                confirm.lower
+                if confirm == "y":
+                    delete([data.at[1, 'id']])
+                    print("Deleted successfully!")
+                    time.sleep(2)
+                    continue
+                else:
+                    print("Cancelling..")
+                    time.sleep(2)
+                    continue
+            
+            # rating
+            if user == 3:
+                print("Insert rate number 1-5!")
                 rate = float(input("> "))
-                rating(data.at[1, 'id'], rate)
+                if rate > 5:
+                    print('Oops.. too much!')
+                    time.sleep(1)
+                    continue
+                print("\nInsert ur review!")
+                review = input("> ")
+                rating(data.at[1, 'id'], rate, review)
                 print("Successfully rated!")
-                print("restarting..")
                 time.sleep(2)
                 continue
             
@@ -168,12 +183,11 @@ What are you gonna do with that?
             continue
         
     elif user == 0:
-        print("Bye bye :(")
+        print("\nBye bye :(")
         exit()
         break
     else:
-        print("Oops, wrong input!")
-        print("restarting..")
+        print("\nOops, wrong input!")
         time.sleep(2)
         continue
         
